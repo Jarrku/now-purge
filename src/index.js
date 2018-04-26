642a1fa6bb35856c3327cb84b4ebce71be278045
Simon VDB <simonvdbroeck@gmail.com>
2018-04-26 20:36:49 +0200

    Use ES6 Class to keep instance data

---
+++ src/index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+"use strict";
+
+const argv = require("yargs")
+  .usage("Usage: $0 <command> [options]")
+  .command("t", "now token")
+  .option("team", {
+    describe: "team id"
+  }).argv;
+
+const Now = require("./now");
+
+let nowClient;
+
+const getDeploymentsWithoutAlias = async () => {
+  const [deployments, aliases] = await Promise.all([
+    nowClient.getDeploys(),
+    nowClient.getAliases()
+  ]);
+
+  return deployments.filter(
+    ({ uid }) =>
+      aliases.findIndex(({ deploymentId }) => deploymentId === uid) === -1
+  );
+};
+
+const removeDeployments = deployments => {
+  if (deployments.length === 0) {
+    console.log("No deployments to remove");
+    return;
+  }
+
+  deployments.forEach(nowClient.removeDeploy);
+};
+
+// you can pass your `package.json` or `now.json` `name` to filter your deployments
+const main = async () => {
+  const { token, team } = argv;
+  nowClient = new Now({ token, team });
+
+  await nowClient.init();
+
+  const deployments = await getDeploymentsWithoutAlias();
+  await removeDeployments(deployments);
+
+  console.log("✅ Succesfully removed unaliases instances");
+};
+
+module.exports = main;
+
+if (!module.parent) {
+  main();
+}
---
+++ src/now.js
@@ -0,0 +1,94 @@
+"use strict";
+
+const fetch = require("node-fetch");
+
+const BASE_URL = "https://api.zeit.co";
+const URLS = {
+  DEPLOYMENT: "/v2/now/deployments",
+  ALIAS: "/v2/now/aliases",
+  TEAMS: "/teams"
+};
+
+const logError = error => {
+  console.log(`${err} ❌`);
+};
+
+class Now {
+  constructor({ token, team }) {
+    this._teamSlug = team;
+    this._headers = {
+      Authorization: `Bearer ${this._token}`,
+      "Content-Type": "application/json"
+    };
+  }
+
+  async init() {
+    //  fetch teamId from slug if needed
+    if (!this._teamSlug) return;
+
+    const url = `${URLS.TEAMS}?slug=${this._teamSlug}`;
+
+    try {
+      const { id } = await this._fetch(url);
+      this._teamId = id;
+    } catch (err) {
+      logError(err);
+    }
+  }
+
+  _createOpts(opts = {}) {
+    let options = Object.assign({}, opts);
+
+    options.headers = Object.assign({}, this._headers, opts.headers);
+
+    return options;
+  }
+
+  _prepareUrl(url) {
+    let urlToUse = `${BASE_URL}${url}`;
+
+    if (this._teamId) {
+      urlToUse = `${urlToUse}?teamId=${this._teamId}`;
+    }
+
+    return urlToUse;
+  }
+
+  async _fetch(url, opts) {
+    const response = await fetch(this._prepareUrl(url), this._createOpts(opts));
+
+    return await response.json();
+  }
+
+  async getDeploys() {
+    try {
+      return this._fetch(URLS.DEPLOYMENT);
+    } catch (err) {
+      logError(err);
+    }
+  }
+
+  async getAliases() {
+    try {
+      return this._fetch(URLS.ALIAS);
+    } catch (err) {
+      logError(err);
+    }
+  }
+
+  async removeDeploy({ uid }) {
+    const url = `${URLS.DEPLOYMENT}/${uid}`;
+    const opts = {
+      method: "DELETE"
+    };
+
+    try {
+      await this.fetch(url, opts);
+      console.log(`${uid} removed ✅`);
+    } catch (err) {
+      logError(`${uid} error`);
+    }
+  }
+}
+
+module.exports = Now;
